cb25de2a33acb9f6d9a1ba85f6064f6e99f3f8a1
glepevedic <gillian.lepevedic@gmail.com>
2021-03-26 10:26:32 +0100

    commentaire Gestion niveau

--- src/LectureFichier/GestionNiveau.java
+++ src/LectureFichier/GestionNiveau.java
@@ -2,14 +2,15 @@ package LectureFichier;
 
 import java.io.FileWriter;
 import java.io.IOException;
-
 import Niveau.Niveau;
 
 /**
- * Class technique
- * la classe gestion de niveau permet d'ouvrir et fermer des fichiers de niveau.
- * Le fichier peut être dans le désordre (les parties //MAP //TEMPS //SCORE) dans l'ordre que l'on veut.
- * Dans cette classe on peut ouvrir un niveau grâce au nom du fichier et/ou l'enregistre grâce à son nom de fichier et le niveau à sauvegarder.
+ * Class technique la classe gestion de niveau permet d'ouvrir et fermer des
+ * fichiers de niveau. Le fichier peut être dans le désordre (les parties //MAP
+ * //TEMPS //SCORE) dans l'ordre que l'on veut. Dans cette classe on peut ouvrir
+ * un niveau grâce au nom du fichier et/ou l'enregistre grâce à son nom de
+ * fichier et le niveau à sauvegarder.
+ * 
  * @author gillian
  *
  */
@@ -17,8 +18,9 @@ public class GestionNiveau {
 	private String nomFichier;
 	private Niveau niveau;
 	private ScannerFichier scannerDeFichier;
+
 	/**
-	 * Constructeur 
+	 * Constructeur
 	 * 
 	 * @param nomFichier
 	 */
@@ -26,70 +28,100 @@ public class GestionNiveau {
 		this.nomFichier = nomFichier;
 	}
 
+	/**
+	 * Cette méthode permet de vérifier si le fichier ouvert correspond au critère
+	 * du jeu. Les entêtes, la taille de la map, la cohérence des valeurs, etc. Il
+	 * faut au minimum un map complet pour que la fonction renvoi un "niveau" non
+	 * null. Le score et le temps ne sont pas obligatoires dans le fichier. Si le
+	 * Score et le temps ne sont pas dans le fichier ma valeur 0 serat affecter aux
+	 * variables.
+	 * 
+	 * @return return le niveau lu dans le fichier qui porte le nom "nomFichier"
+	 *         declarer dans le constructeur.
+	 */
 	public Niveau ouvrirNiveau() {
-		scannerDeFichier = new ScannerFichier(nomFichier);
 		niveau = new Niveau();
-		try {
-			if (scannerDeFichier.getScannerDeFichier() != null) {
-				for (int i = 0; i < 3; i++) {
-					switch (scannerDeFichier.lectureLigne()) {
 
-					case "//MAP":
-						niveau.setMap(lectureMap());
-						break;
-					case "//TEMPS":
-						niveau.setMeilleurTempsEnSeconde(lectureTemps());
-						break;
-					case "//SCORE":
-						niveau.setMeilleurScore(lectureScore());
-						break;
-					case "":
-						i--;
-						break;
-					default:
-						System.out.println("Erreur");
-					}
-					
+		try {
+			scannerDeFichier = new ScannerFichier(nomFichier);
+			scannerDeFichier.getScannerDeFichier();
+			for (int i = 0; i < 3; i++) {
+				switch (scannerDeFichier.lectureLigne()) {
+
+				case "//MAP":
+					niveau.setMap(lectureMap());
+					break;
+				case "//TEMPS":
+					niveau.setMeilleurTempsEnSeconde(lectureTemps());
+					break;
+				case "//SCORE":
+					niveau.setMeilleurScore(lectureScore());
+					break;
+				case "":
+					i--;
+					break;
+				default:
+					throw new Exception("Erreur : entete non reconu");
 				}
-				scannerDeFichier.close();
-				System.out.println("Fin lecture niveau");
-			}else {
-				throw new Exception("Erreur : scanner fermer");
+
 			}
+			System.out.println("Fin lecture niveau");
 
 		} catch (Exception e) {
 			System.out.println(e);
-			niveau = null;
+			if (niveau.getMap() == null) {
+				niveau = null;
+			}
 		}
 
+		scannerDeFichier.close();
 		return niveau;
 	}
 
-	private boolean[][] lectureMap() {
+	/**
+	 * Cette méthode est appelée si on rencontre les mots "//MAP". Elle lit le parti
+	 * du fichier contenant la map et vérifie plusieurs critères. La longueur, la
+	 * largeur, les valeursSi la map répond pas à un seul de ces critères la lecture
+	 * est abandonnée
+	 * 
+	 * @return retourne un tableau de boolean a deux dimension.
+	 * @throws Exception Si la map de corespond pas au critère.
+	 */
+	private boolean[][] lectureMap() throws Exception {
 		boolean[][] map = new boolean[Niveau.getLargeurMap()][Niveau.getLongueurMap()];
 		String ligneString;
-		try {
 
-			for (int largeur = 0; largeur < Niveau.getLargeurMap(); largeur++) {
-				ligneString = scannerDeFichier.lectureLigne();
+		for (int largeur = 0; largeur < Niveau.getLargeurMap(); largeur++) {
+			ligneString = scannerDeFichier.lectureLigne();
 
-				if (ligneString.length() != Niveau.getLongueurMap()) {
-					throw new Exception("Erreur : La ligne " + ligneString + " n'est pas a la bonne taille");
-				}
+			if (ligneString == null) {
+				throw new Exception("Erreur : Ligne vide");
+			}
 
-				for (int longueur = 0; longueur < Niveau.getLongueurMap(); longueur++) {
+			if (ligneString.length() != Niveau.getLongueurMap()) {
+				throw new Exception("Erreur : La ligne " + ligneString + " n'est pas a la bonne taille");
+			}
+
+			for (int longueur = 0; longueur < Niveau.getLongueurMap(); longueur++) {
+				if (ligneString.charAt(longueur) == '0' || ligneString.charAt(longueur) == '1') {
 					map[largeur][longueur] = ligneString.charAt(longueur) == '1';
+				} else {
+					throw new Exception(
+							"Erreur : le caractere " + ligneString.charAt(longueur) + " est interdit dans une map");
 				}
 
 			}
-		} catch (Exception e) {
-			System.out.println("Erreur : contenue pas au norme");
-			System.out.println(e);
-			map = null;
+
 		}
+
 		return map;
 	}
 
+	/**
+	 * Cette méthode est appelée si on rencontre les mots "//TEMPS".
+	 * 
+	 * @return retourne le temps lu par le fichier sinon retourne 0
+	 */
 	private int lectureTemps() {
 		int temps;
 		try {
@@ -99,14 +131,24 @@ public class GestionNiveau {
 			System.out.println(e);
 			temps = 0;
 		}
+
 		return temps;
 	}
 
+	/**
+	 * Cette méthode est appelée si on rencontre les mots "//SCORE".
+	 * 
+	 * @return retourne le score lu par le fichier sinon retourne 0
+	 */
 	private int lectureScore() {
 		int score;
 		try {
 			score = Integer.parseInt(scannerDeFichier.lectureLigne());
 
+		} catch (NullPointerException e) {
+			System.out.println("Erreur : pas de Score");
+			System.out.println(e);
+			score = 0;
 		} catch (Exception e) {
 			System.out.println("Erreur : lecture Score impossible");
 			System.out.println(e);
@@ -115,6 +157,13 @@ public class GestionNiveau {
 		return score;
 	}
 
+	/**
+	 * Cette méthode permet de sauvegarder le niveau passé en paramètre dans le
+	 * fichier déclarer dans le constructeur. Le fichier est d'abord vidé. Puis
+	 * remplis avec la structure //MAP //TEMPS //SCRORE
+	 * 
+	 * @param niveau il serat envoyé dans le fichier "nomfichier"
+	 */
 	public void sauvegarderNiveau(Niveau niveau) {
 		System.out.println("Debut ecriture");
 		viderFichier();
@@ -124,6 +173,9 @@ public class GestionNiveau {
 		System.out.println("Fin ecriture");
 	}
 
+	/**
+	 * vide le fichier portant le nom "nomFichier"
+	 */
 	private void viderFichier() {
 		try (FileWriter lefichier = new FileWriter(nomFichier);) {
 			lefichier.write("");
@@ -132,6 +184,11 @@ public class GestionNiveau {
 		}
 	}
 
+	/**
+	 * Remplie le ficher avec la map sous forme de tableau. Avec un entête //MAP
+	 * 
+	 * @param map
+	 */
 	private void ecrireMap(boolean[][] map) {
 		try (FileWriter lefichier = new FileWriter(nomFichier, true);) {
 			lefichier.write("//MAP\n");
@@ -151,6 +208,11 @@ public class GestionNiveau {
 		}
 	}
 
+	/**
+	 * Remplie le ficher avec la map sous forme de tableau. Avec un entête //TEMPS
+	 * 
+	 * @param temps
+	 */
 	private void ecrireTemp(int temps) {
 		try (FileWriter lefichier = new FileWriter(nomFichier, true);) {
 			lefichier.write("//TEMPS\n");
@@ -162,8 +224,12 @@ public class GestionNiveau {
 
 	}
 
+	/**
+	 * Remplie le ficher avec la map sous forme de tableau. Avec un entête //SCORE
+	 * 
+	 * @param score
+	 */
 	private void ecrireScore(int score) {
-
 		try (FileWriter lefichier = new FileWriter(nomFichier, true);) {
 			lefichier.write("//SCORE\n");
 			lefichier.write(String.valueOf(score));
--- src/LectureFichier/ScannerFichier.java
+++ src/LectureFichier/ScannerFichier.java
@@ -1,63 +1,34 @@
 package LectureFichier;
 
 import java.io.FileInputStream;
-import java.util.NoSuchElementException;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ScannerFichier {
 	private Scanner scannerDeFichier;
-	
-	public  ScannerFichier(String nomfic) {
-		try {
-			FileInputStream file = new FileInputStream(nomfic);
-			scannerDeFichier = new Scanner(file);
-			System.out.println("Fichier ouvert");
-			
-		} catch (java.io.FileNotFoundException e) {
-			System.out.println("Erreur : Fichier introuvable");
-			System.out.println(e);
-		} catch (Exception e) {
-			System.out.println("Erreur : Lecture imposible");
-			System.out.println(e);
-			scannerDeFichier =null;
-		}
+
+	public ScannerFichier(String nomfic) throws FileNotFoundException {
+		FileInputStream file = new FileInputStream(nomfic);
+		scannerDeFichier = new Scanner(file);
+		System.out.println("Fichier ouvert");
 	}
-	
+
 	public String lectureLigne() throws Exception {
-		String ligne =null;
-		try {
-			if (scannerDeFichier.hasNext()) {
-					ligne = scannerDeFichier.nextLine();
-			}else {
-				throw new Exception("Erreur : Pas de ligne a lire");
-			}
-		}catch(NoSuchElementException e) {
-			System.out.println("Erreur : Imposible de lire la ligne");
-			System.out.println(e);
-			ligne = null;
-		}catch(IllegalStateException e) {
-			System.out.println("Erreur : Scanner fermer");
-			System.out.println(e);
-			ligne = null;
-		}catch(NullPointerException e) {
-			System.out.println("Erreur : Scanner null");
-			System.out.println(e);
-			ligne = null;
-		}catch(Exception e) {
-			System.out.println("Erreur : Pas de ligne a lire");
-			System.out.println(e);
-			ligne = null;
+		String ligne = null;
+		if (scannerDeFichier.hasNext()) {
+			ligne = scannerDeFichier.nextLine();
+		} else {
+			throw new Exception("Erreur : Pas de ligne a lire");
 		}
-			
-		
+
 		return ligne;
 	}
-	
+
 	void close() {
 		try {
 			scannerDeFichier.close();
-		}catch(NullPointerException e) {
-			System.out.println("Erreur : Scanner null");
+		} catch (NullPointerException e) {
+			System.out.println("Erreur : Fermer");
 			System.out.println(e);
 		}
 	}
@@ -65,5 +36,4 @@ public class ScannerFichier {
 	public Scanner getScannerDeFichier() {
 		return scannerDeFichier;
 	}
-		
 }
